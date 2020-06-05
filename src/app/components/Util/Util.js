6aeddf3d92744bcb91d01c44f2df3c7ef3999bc1
Kevin <kevin.zamperetti@dbccompany.com.br>
2020-06-05 19:58:55 -0300

    Create method for validate cpf

--- src/app/components/Util/Util.js
+++ src/app/components/Util/Util.js
@@ -267,6 +267,29 @@ export default class Util extends React.Component {
     }
   }
 
+  validateDocumentNumber(documentNumber) {
+    let Soma;
+    let Resto;
+    Soma = 0;
+    let i = 0;
+  if (documentNumber == "00000000000") return false;
+     
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(documentNumber.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+   
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(documentNumber.substring(9, 10)) ) return false;
+   
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(documentNumber.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+   
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(documentNumber.substring(10, 11) ) ) return false;
+    return true;
+}
+
+
 // ========== Toast Contents: ============
 // eslint-disable-next-line react/prop-types
 contentSuccess(message) {
